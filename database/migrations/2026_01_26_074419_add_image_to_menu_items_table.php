a27e0157797e1d9160ed08652919d5ea6beae2e2
NessimAboodi <nessim.aboodi@lamache.org>
2026-01-26 09:30:30 +0100

    push

--- database/migrations/2026_01_26_074419_add_image_to_menu_items_table.php
+++ database/migrations/2026_01_26_074419_add_image_to_menu_items_table.php
@@ -1,6 +1,30 @@
-public function up(): void
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
 {
-Schema::table('menu_items', function (Blueprint $table) {
-$table->string('image')->nullable()->after('price'); // Ajoute le champ image après le prix
-});
-}
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            // Ajoute la colonne image après le prix
+            $table->string('image')->nullable()->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('menu_items', function (Blueprint $table) {
+            // Supprime la colonne si on annule la migration
+            $table->dropColumn('image');
+        });
+    }
+};
